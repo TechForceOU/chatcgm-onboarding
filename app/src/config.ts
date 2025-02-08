f73c1f1271c4be838da007fdfebd747e6628ee3c
Kirill Gusev <kirill@techforce.pro>
2025-02-08 01:21:43 +0100

    Updated onboarding content to ChatCGM   features

--- app/src/config.ts
+++ app/src/config.ts
@@ -9,46 +9,58 @@ export default defineConfig({
     {
       slides: [
         // intro
-        {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/yoda_thinking.tgs'),
-            size: 200,
-          },
-          shape: 'square',
-          pagination: 'count',
-          title: 'ChatGPT (GPT-4) now in Telegram!',
-          description: "Most advanced AI powered by <a href='https://openai.com' target='_blank'>OpenAI</a>",
-          button: 'Next',
-        },
+        // {
+        //   media: {
+        //     type: 'sticker',
+        //     src: import('./assets/stickers/yoda_heart.tgs'),
+        //     size: 200,
+        //   },
+        //   shape: 'square',
+        //   pagination: 'count',
+        //   title: 'Welcome to ChatCGM',
+        //   description: "Most advanced AI for the efficient diabetes management",
+        //   button: 'Next',
+        // },
 
         // video 1
         {
           media: {
             type: 'video',
-            src: import('./assets/videos/chatgpt_1.mp4'),
-            poster: import('./assets/img/chatgpt_1_poster.webp'),
+            src: import('./assets/videos/carb_estimation.mp4'),
+            poster: import('./assets/img/food_photo_poster.webp'),
             style: 'aspect-ratio: 780/936',
           },
           shape: 'rounded',
           pagination: 'count',
-          title: 'Ask any questions',
-          description: "Get instant smart answers",
+          title: 'Scan Food, Know Carbs',
+          description: "AI-powered carb counting at your fingertips",
           button: 'Next',
         },
-
-        // video 2
         {
           media: {
             type: 'video',
-            src: import('./assets/videos/chatgpt_2.mp4'),
-            poster: import('./assets/img/chatgpt_2_poster.webp'),
+            src: import('./assets/videos/estimation_correction.mp4'),
+            poster: import('./assets/img/empty_chat_poster.webp'),
             style: 'aspect-ratio: 780/936',
           },
           shape: 'rounded',
           pagination: 'count',
-          title: 'Send Voice Messages',
-          description: "90+ languages are supported",
+          title: 'Adjust on the way',
+          description: "Make corrections to the estimation by specifying ingridients or cooking method",
+          button: 'Next',
+        },
+        // video 3
+        {
+          media: {
+            type: 'video',
+            src: import('./assets/videos/voice_message.mp4'),
+            poster: import('./assets/img/empty_chat_poster.webp'),
+            style: 'aspect-ratio: 780/936',
+          },
+          shape: 'rounded',
+          pagination: 'count',
+          title: 'The easiest way to track your meals, insulin and excercise',
+          description: "Send voice or text command to add records directly to your Nightscout",
           button: {
             content: 'Send your first message!',
             to: '/close',
